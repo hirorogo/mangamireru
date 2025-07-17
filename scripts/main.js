944f081f2db889455d41f5485bcfacb837631e96
hirorogo <hiro114514@proton.me>
2025-07-17 10:39:49 +0900

    Refactor code structure for improved readability and maintainability

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 addEventListener("DOMContentLoaded", () => {
-    const search = document.querySelector('#search');
-    const kensaku = document.querySelector("#search_text");
-
-    search.addEventListener('click', () => {
-        alert(`${kensaku.value}が入力されました`);
-      });
+  const search = document.querySelector('#search{');
+  const kensaku = document.querySelector("#search_text");
+  
+  search.addEventListener('click', () => {
+    alert(`${kensaku.value}が入力されました`);
+  });
 });
