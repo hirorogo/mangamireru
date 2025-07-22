247961e416828f0788a31bf8c82414746cf4b1f1
hirorogo <hiro114514@proton.me>
2025-07-22 22:17:25 +0900

    index.htmlのresultListのIDをresult_listに変更し、main.jsでの参照を更新。search機能の結果を表示するためのリストを整備。main.pyにfaviconのエンドポイントを追加し、PDFリスト取得時に隠しファイルを除外。main.cssにresult_listのマージンを追加。不要なファイルを削除。

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import ORJSONResponse, FileResponse
+import json
 
 PORT = 8000
 
@@ -12,9 +13,14 @@ app = FastAPI(default_response_class=ORJSONResponse)
 app.mount("/books", StaticFiles(directory="books", html=True), name="static")
 app.mount("/styles", StaticFiles(directory="styles"), name="styles")
 app.mount("/scripts", StaticFiles(directory="scripts"), name="scripts")
+
 @app.get("/")
 async def index():
     return FileResponse("index.html")
+
+@app.get("/favicon.png")
+async def index():
+    return FileResponse("favicon.png")
 
 # 本の一覧
 @app.get("/list-books")
@@ -22,10 +28,24 @@ async def booksList():
     books_dir = "./books"
     if not os.path.exists(books_dir):
         raise HTTPException(404, "Books directory not found")
-    files = [f for f in os.listdir(books_dir) if f.endswith(".pdf")]
+    files = [f for f in os.listdir(books_dir) if not f.startswith(".")]
     books = []
+    
+    def wrapPath(path: str):
+        return path.replace(".", "")
+    
     for file in files:
-    return files
+        with open(f"{books_dir}/{file}/metadata.json", "r", encoding="utf-8") as f:
+            metadata = json.load(f)
+        book = {
+            "thumb": wrapPath(f"{books_dir}/{file}/thumb.png"),
+            "pdf": wrapPath(f"{books_dir}/{file}/content.pdf"),
+            "metadata": metadata
+        }
+        
+        books.append(book)
+        
+    return books
 
 
 @app.get("/pdfjs/{path:path}")
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 addEventListener("DOMContentLoaded", () => {
   const search = document.querySelector('#search');
   const kensaku = document.querySelector("#search_text");
+  const resultList = document.querySelector("#result_list");
 
   search.addEventListener('click', async () => {
     const searchResponse = await fetch("list-books", {
@@ -9,7 +10,16 @@ addEventListener("DOMContentLoaded", () => {
 
     if (searchResponse.ok) {
       const searchResult = await searchResponse.json();
-      alert(`Debug: ${JSON.stringify(searchResult)}`)
+      console.table(searchResult);
+      while (resultList.firstChild) resultList.firstChild.remove();
+
+      for (const book of searchResult) {
+        const bookEl = document.createElement("li");
+
+        bookEl.textContent = JSON.stringify(book);
+
+        resultList.appendChild(bookEl)
+      }
     }else {
       alert(`検索においてエラーが発生しました。(${searchResponse.statusText})`)
     }
